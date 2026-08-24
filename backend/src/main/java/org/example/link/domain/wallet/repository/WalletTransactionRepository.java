1f8e338a622903407607e21a1e265b0f6d276fb3
wlsdn020416 <wlsdn020416@gmail.com>
2026-08-24 15:47:17 +0900

    feat: wallet, transection Repository 생성

---
+++ backend/src/main/java/org/example/link/domain/wallet/repository/WalletTransactionRepository.java
@@ -0,0 +1,14 @@
+package org.example.link.domain.wallet.repository;
+
+import org.example.link.domain.wallet.entity.WalletTransactionEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WalletTransactionRepository
+        extends JpaRepository<WalletTransactionEntity, Long> {
+    Page<WalletTransactionEntity> findByWalletIdOrderByCreatedAtDesc(
+            Long walletId,
+            Pageable pageable
+    );
+}
