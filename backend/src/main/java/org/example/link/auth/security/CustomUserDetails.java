1160968a321ce5670152c198155aaea41a2befaf
wlsdn020416 <wlsdn020416@gmail.com>
2026-08-24 14:40:58 +0900

    refactor: jwt 중복 조회 수정중

--- backend/src/main/java/org/example/link/auth/security/CustomUserDetails.java
+++ backend/src/main/java/org/example/link/auth/security/CustomUserDetails.java
@@ -1,4 +1,28 @@
 package org.example.link.auth.security;
 
-public class CustomUserDetails {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private final Long userId;
+    private final String email;
+    @Override
+    public String getUsername() {
+        return email;
+    }
+    @Override
+    public String getPassword() {
+        return "";
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
 }
