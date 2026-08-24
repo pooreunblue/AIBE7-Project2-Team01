1bfe3f58c28dd52050737d00b7b926f7206965d4
bsh <mjbsh02@mju.ac.kr>
2026-08-24 17:13:14 +0900

    feat: 요청글 엔티티 구현

---
+++ backend/src/main/java/org/example/link/domain/request/entity/RequestPostEntity.java
@@ -0,0 +1,47 @@
+package org.example.link.domain.request.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.link.common.entity.BaseEntity;
+import org.example.link.domain.category.entity.CategoryEntity;
+import org.example.link.domain.user.entity.UserEntity;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "request_posts")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class RequestPostEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "request_post_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private CategoryEntity category;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Long budget_max;
+
+    @Column(nullable = false)
+    private Long budget_min;
+
+    private String status;
+    private BigDecimal ai_confidence;
+}
