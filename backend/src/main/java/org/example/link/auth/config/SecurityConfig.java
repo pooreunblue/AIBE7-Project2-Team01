10616453964f2c3527e3fc5a37bc48b003b3ccad
wlsdn020416 <wlsdn020416@gmail.com>
2026-08-24 09:28:51 +0900

    feat: swagger config

--- backend/src/main/java/org/example/link/auth/config/SecurityConfig.java
+++ backend/src/main/java/org/example/link/auth/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package org.example.link.auth.config;
 
+import lombok.RequiredArgsConstructor;
 import org.example.link.auth.jwt.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -12,9 +14,13 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtFilter jwtFilter;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 // JWT 방식이면 세션 사용 안 함
                 .sessionManagement(session ->
