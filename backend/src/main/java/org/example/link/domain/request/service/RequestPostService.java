08e5220ba32a6d1e38da0cdcc37af8563192b238
bsh <mjbsh02@mju.ac.kr>
2026-08-24 18:26:43 +0900

    feat: 요청글 서비스 create 로직 구현

--- backend/src/main/java/org/example/link/domain/request/service/RequestPostService.java
+++ backend/src/main/java/org/example/link/domain/request/service/RequestPostService.java
@@ -1,4 +1,34 @@
 package org.example.link.domain.request.service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.link.common.exception.CustomException;
+import org.example.link.common.exception.ErrorCode;
+import org.example.link.domain.category.entity.CategoryEntity;
+import org.example.link.domain.category.repository.CategoryRepository;
+import org.example.link.domain.request.dto.RequestPostRequestDto;
+import org.example.link.domain.request.entity.RequestPostEntity;
+import org.example.link.domain.request.repository.RequestPostRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class RequestPostService {
+    private final RequestPostRepository requestPostRepository;
+    private final CategoryRepository categoryRepository;
+
+    @Transactional
+    public RequestPostEntity create(RequestPostRequestDto requestPostRequestDto) {
+        CategoryEntity category = categoryRepository.findById(requestPostRequestDto.categoryId())
+                .orElseThrow(() -> new CustomException(ErrorCode.CATEGORY_NOT_FOUND));
+        RequestPostEntity requestPostEntity = RequestPostEntity.builder()
+                .category(category)
+                .title(requestPostRequestDto.title())
+                .content(requestPostRequestDto.content())
+                .budget_max(requestPostRequestDto.budget_max())
+                .budget_min(requestPostRequestDto.budget_min())
+                .build();
+        return requestPostRepository.save(requestPostEntity);
+    }
 }
