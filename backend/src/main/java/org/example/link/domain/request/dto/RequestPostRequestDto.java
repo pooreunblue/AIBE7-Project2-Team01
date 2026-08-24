31f7426342e39288ff7de22b08c5b8cad9d18f38
bsh <mjbsh02@mju.ac.kr>
2026-08-24 18:24:58 +0900

    refactor: 요청글 Dto 수정

--- backend/src/main/java/org/example/link/domain/request/dto/RequestPostRequestDto.java
+++ backend/src/main/java/org/example/link/domain/request/dto/RequestPostRequestDto.java
@@ -1,20 +1,14 @@
 package org.example.link.domain.request.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 import org.example.link.domain.request.entity.RequestPostEntity;
 
 public record RequestPostRequestDto(
         @NotBlank String title,
         @NotBlank String content,
+        Long categoryId,
         @NotBlank Long budget_max,
         @NotBlank Long budget_min
 ) {
-    public RequestPostEntity toEntity() {
-        return RequestPostEntity.builder()
-                .title(title)
-                .content(content)
-                .budget_max(budget_max)
-                .budget_min(budget_min)
-                .build();
-    }
 }
