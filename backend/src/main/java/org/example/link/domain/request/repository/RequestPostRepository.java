c0e3d8435d13d9c328ed6df2f2553d4bc293bc17
bsh <mjbsh02@mju.ac.kr>
2026-08-25 09:17:33 +0900

    feat: 요청 게시글 조회 리포지토리 추가

--- backend/src/main/java/org/example/link/domain/request/repository/RequestPostRepository.java
+++ backend/src/main/java/org/example/link/domain/request/repository/RequestPostRepository.java
@@ -3,5 +3,8 @@ package org.example.link.domain.request.repository;
 import org.example.link.domain.request.entity.RequestPostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RequestPostRepository extends JpaRepository<RequestPostEntity, Long> {
+    List<RequestPostEntity> findAllById(Long userId);
 }
