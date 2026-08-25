cce16fe429796e816825efcb9282ad91101bb8ec
wlsdn020416 <wlsdn020416@gmail.com>
2026-08-25 09:39:19 +0900

    feat: express

--- backend/src/main/java/org/example/link/auth/config/CorsConfig.java
+++ backend/src/main/java/org/example/link/auth/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
-                List.of("http://localhost:5173")
+                List.of("http://localhost:3000")
         );
         configuration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
