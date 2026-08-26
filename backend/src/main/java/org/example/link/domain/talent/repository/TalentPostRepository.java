e2926a1d26ad8cd876f3d25bf4ed68ed90779136
bsh <mjbsh02@mju.ac.kr>
2026-08-26 12:29:29 +0900

    feat: 재능 게시글 리포지토리 추가

---
+++ backend/src/main/java/org/example/link/domain/talent/repository/TalentPostRepository.java
@@ -0,0 +1,7 @@
+package org.example.link.domain.talent.repository;
+
+import org.example.link.domain.talent.entity.TalentPostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TalentPostRepository extends JpaRepository<TalentPostEntity, Long> {
+}
