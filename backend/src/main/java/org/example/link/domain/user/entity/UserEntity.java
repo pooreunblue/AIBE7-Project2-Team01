514149d12257b1fa53fadc42a4c30d410fd85488
wlsdn020416 <wlsdn020416@gmail.com>
2026-08-26 10:00:32 +0900

    chore: user entity 생성자 구분, 로그인 방식 enum 타입 추가

--- backend/src/main/java/org/example/link/domain/user/entity/UserEntity.java
+++ backend/src/main/java/org/example/link/domain/user/entity/UserEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.link.auth.config.AuthProvider;
 import org.example.link.common.entity.BaseEntity;
 
 @Entity
@@ -19,19 +20,50 @@ public class UserEntity extends BaseEntity {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
+    @Column
     private String password;
 
     @Column(nullable = false, unique = true)
     private String nickname;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AuthProvider provider = AuthProvider.LOCAL;
+
     public UserEntity(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
         this.nickname = nickname;
+        this.provider = AuthProvider.LOCAL;
+    }
+
+    public static UserEntity create(
+            String email,
+            String password,
+            String nickname
+    ) {
+        return new UserEntity(
+                email,
+                password,
+                nickname
+        );
+    }
+
+    // TODO : 닉네임 중복 시 랜덤 suffix 추가
+    public static UserEntity createSocialUser(
+            String email,
+            String nickname
+    ) {
+        UserEntity user = new UserEntity();
+        user.email = email;
+        user.password = null;
+        user.nickname = nickname;
+        user.provider = AuthProvider.GOOGLE;
+        return user;
     }
 
     public void updateNickname(String nickname) {
         this.nickname = nickname;
     }
 }
+
