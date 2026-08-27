95704dc98a59babe8233502fc5663ae760b888df
bsh <mjbsh02@mju.ac.kr>
2026-08-27 16:25:43 +0900

    refactor: 재능글 서비스 중복 코드 정리

--- backend/src/main/java/org/example/link/domain/talent/service/TalentPostService.java
+++ backend/src/main/java/org/example/link/domain/talent/service/TalentPostService.java
@@ -15,6 +15,7 @@ import org.example.link.domain.talent.repository.TalentPostRepository;
 import org.example.link.domain.talent.util.TalentPostStatus;
 import org.example.link.domain.user.entity.UserEntity;
 import org.example.link.domain.user.repository.UserRepository;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -34,24 +35,11 @@ public class TalentPostService {
 
     @Transactional
     public TalentPostEntity create(TalentPostRequestDto talentPostRequestDto, CustomUserDetails userDetails) {
-        Long userId = userDetails.getUserId();
-        UserEntity user = userRepository.findById(userId)
-                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
-        CategoryEntity category = categoryRepository.findById(talentPostRequestDto.categoryId())
-                .orElseThrow(() -> new CustomException(ErrorCode.CATEGORY_NOT_FOUND));
-        PortfolioEntity portfolio = portfolioRepository.findById(talentPostRequestDto.portfolioId())
-                .orElseThrow(() -> new CustomException(ErrorCode.PORTFOLIO_NOT_FOUND));
-        TalentPostEntity talentPostEntity = TalentPostEntity.builder()
-                .user(user)
-                .category(category)
-                .title(talentPostRequestDto.title())
-                .content(talentPostRequestDto.content())
-                .price(talentPostRequestDto.price())
-                .estimatedDuration(talentPostRequestDto.estimatedDuration())
-                .durationUnit(talentPostRequestDto.durationUnit())
-                .portfolio(portfolio)
-                .status(TalentPostStatus.ACTIVE)
-                .build();
+        Long userId = getUserId(userDetails);
+        UserEntity user = getUser(userId);
+        CategoryEntity category = getCategory(talentPostRequestDto);
+        PortfolioEntity portfolio = getPortfolio(talentPostRequestDto);
+        TalentPostEntity talentPostEntity = createTalentPost(talentPostRequestDto, user, category, portfolio);
         return talentPostRepository.save(talentPostEntity);
     }
 
@@ -60,10 +48,9 @@ public class TalentPostService {
     }
 
     public TalentPostEntity readOne(Long talentPostId) {
-        return talentPostRepository.findById(talentPostId)
-                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
+        return getTalentPostEntity(talentPostId);
     }
-
+    
     @Transactional(readOnly = true)
     public Page<TalentPostEntity> search(
             String keyword,
@@ -77,16 +64,77 @@ public class TalentPostService {
             Long talentPostId,
             TalentPostRequestDto talentPostRequestDto,
             CustomUserDetails userDetails) throws AccessDeniedException {
-        Long userId = userDetails.getUserId();
-        TalentPostEntity talentPostEntity = talentPostRepository.findById(talentPostId)
-                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
-        if (!talentPostEntity.getUser().getId().equals(userId)) {
-            throw new AccessDeniedException("작성자만 수정할 수 있습니다.");
-        }
-        CategoryEntity category = categoryRepository.findById(talentPostRequestDto.categoryId())
+        Long userId = getUserId(userDetails);
+        TalentPostEntity talentPostEntity = getTalentPostEntity(talentPostId);
+        validateAuth(talentPostEntity, userId);
+        CategoryEntity category = getCategory(talentPostRequestDto);
+        PortfolioEntity portfolio = getPortfolio(talentPostRequestDto);
+        updateTalentPost(talentPostRequestDto, talentPostEntity, category, portfolio);
+        return talentPostEntity;
+    }
+
+    @Transactional
+    public void delete(Long talentPostId, CustomUserDetails userDetails) throws AccessDeniedException {
+        Long userId = getUserId(userDetails);
+        TalentPostEntity talentPostEntity = getTalentPostEntity(talentPostId);
+        validateAuth(talentPostEntity, userId);
+        talentPostRepository.delete(talentPostEntity);
+    }
+
+    @Transactional
+    public TalentPostEntity inactiveStatus(Long talentPostId, CustomUserDetails userDetails) throws AccessDeniedException {
+        Long userId = getUserId(userDetails);
+        TalentPostEntity talentPostEntity = getTalentPostEntity(talentPostId);
+        validateAuth(talentPostEntity, userId);
+        talentPostEntity.inactiveStatus();
+        return talentPostEntity;
+    }
+
+    private Long getUserId(CustomUserDetails userDetails) {
+        return userDetails.getUserId();
+    }
+
+    private @NonNull UserEntity getUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    private @NonNull CategoryEntity getCategory(TalentPostRequestDto talentPostRequestDto) {
+        return categoryRepository.findById(talentPostRequestDto.categoryId())
                 .orElseThrow(() -> new CustomException(ErrorCode.CATEGORY_NOT_FOUND));
-        PortfolioEntity portfolio = portfolioRepository.findById(talentPostRequestDto.portfolioId())
-                        .orElseThrow(() -> new CustomException(ErrorCode.PORTFOLIO_NOT_FOUND));
+    }
+
+    private @NonNull PortfolioEntity getPortfolio(TalentPostRequestDto talentPostRequestDto) {
+        return portfolioRepository.findById(talentPostRequestDto.portfolioId())
+                .orElseThrow(() -> new CustomException(ErrorCode.PORTFOLIO_NOT_FOUND));
+    }
+
+    private TalentPostEntity createTalentPost(TalentPostRequestDto talentPostRequestDto, UserEntity user, CategoryEntity category, PortfolioEntity portfolio) {
+        return TalentPostEntity.builder()
+                .user(user)
+                .category(category)
+                .title(talentPostRequestDto.title())
+                .content(talentPostRequestDto.content())
+                .price(talentPostRequestDto.price())
+                .estimatedDuration(talentPostRequestDto.estimatedDuration())
+                .durationUnit(talentPostRequestDto.durationUnit())
+                .portfolio(portfolio)
+                .status(TalentPostStatus.ACTIVE)
+                .build();
+    }
+
+    private @NonNull TalentPostEntity getTalentPostEntity(Long talentPostId) {
+        return talentPostRepository.findById(talentPostId)
+                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
+    }
+
+    private void validateAuth(TalentPostEntity talentPostEntity, Long userId) throws AccessDeniedException {
+        if (!talentPostEntity.getUser().getId().equals(userId)) {
+            throw new CustomException(ErrorCode.POST_ACCESS_DENIED);
+        }
+    }
+
+    private void updateTalentPost(TalentPostRequestDto talentPostRequestDto, TalentPostEntity talentPostEntity, CategoryEntity category, PortfolioEntity portfolio) {
         talentPostEntity.update(
                 talentPostRequestDto.title(),
                 talentPostRequestDto.content(),
@@ -96,29 +144,5 @@ public class TalentPostService {
                 talentPostRequestDto.durationUnit(),
                 portfolio
         );
-        return talentPostEntity;
-    }
-
-    @Transactional
-    public void delete(Long talentPostId, CustomUserDetails userDetails) throws AccessDeniedException {
-        Long userId = userDetails.getUserId();
-        TalentPostEntity talentPostEntity = talentPostRepository.findById(talentPostId)
-                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
-        if (!talentPostEntity.getUser().getId().equals(userId)) {
-            throw new AccessDeniedException("작성자만 삭제할 수 있습니다.");
-        }
-        talentPostRepository.delete(talentPostEntity);
-    }
-
-    @Transactional
-    public TalentPostEntity inactiveStatus(Long talentPostId, CustomUserDetails userDetails) throws AccessDeniedException {
-        Long userId = userDetails.getUserId();
-        TalentPostEntity talentPostEntity = talentPostRepository.findById(talentPostId)
-                .orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
-        if (!talentPostEntity.getUser().getId().equals(userId)) {
-            throw new AccessDeniedException("작성자만 상태를 변경할 수 있습니다.");
-        }
-        talentPostEntity.inactiveStatus();
-        return talentPostEntity;
     }
 }
